7c75d80b134afd17221664bc42f65c657e9f34a2
bingbang <44920040@qq.com>
2019-11-21 20:26:53 +0800

    Initial commit

---
+++ src/main/java/com/github/startzyp/entity/GoodEntity.java
@@ -0,0 +1,35 @@
+package com.github.startzyp.entity;
+
+public class GoodEntity {
+    private String orderid;
+    private String cmd;
+
+    @Override
+    public String toString() {
+        return "GoodEntity{" +
+                "orderid='" + orderid + '\'' +
+                ", cmd='" + cmd + '\'' +
+                '}';
+    }
+
+    public String getOrderid() {
+        return orderid;
+    }
+
+    public void setOrderid(String orderid) {
+        this.orderid = orderid;
+    }
+
+    public String getCmd() {
+        return cmd;
+    }
+
+    public void setCmd(String cmd) {
+        this.cmd = cmd;
+    }
+
+    public GoodEntity(String orderid, String cmd) {
+        this.orderid = orderid;
+        this.cmd = cmd;
+    }
+}
